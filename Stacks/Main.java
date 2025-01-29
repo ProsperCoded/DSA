6e1803bdee9b32e8b323961432e71c5c2b4fcea8
ProsperCoded <enweremproper@gmail.com>
2025-01-29 10:33:26 +0100

    Implemented my custom stack and methods

--- Stacks/Main.java
+++ Stacks/Main.java
@@ -8,9 +8,17 @@ import java.util.Stack;
 public class Main {
   // public static 
   // checking if string i                s a balanced expression
-  public static void main                (String[] args) {
-    var stack = new StackApplications("abc(d)]");
-    
-    System.out.println(stack.checkExpression());
+  public static void main(String[] args) {
+    var stack = new CustomStack();
+    stack.push(3);
+    stack.push(3);
+    stack.push(3);
+    stack.push(3);
+    stack.push(5);
+
+    System.out.println(java.util.Arrays.toString(stack.get()));
+    System.out.println(stack.peek());
+  
+  
   }
 }
